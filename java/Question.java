62a22e1319ace78a36c55df1765bb4899483d6b2
Omkarbarad <164455064+Omkarbarad@users.noreply.github.com>
2025-01-26 21:23:01 +0530

    project uploaded

---
+++ java/Question.java
@@ -0,0 +1,18 @@
+public class Question 
+{
+
+    public static void main(String args[])
+    {    
+        int number=1;
+        int number2=12;
+        while(number<number2)  
+            {
+                number=number+1;
+            }
+        System.out.println(number);
+    }
+     
+    
+    
+
+}
